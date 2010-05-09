266e3452fbaa80155edebcbecb4fa8c75e18b1b2
Chad Granum <exodist7@gmail.com>
2010-05-09 15:58:14 -0700

    Initial commit

---
+++ t/Fennec.t
@@ -0,0 +1,24 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use Cwd;
+
+use Fennec::Util::Alias qw/
+    Fennec::Runner
+/;
+
+'Fennec::Runner'->init(
+    parallel_files => 2,
+    parallel_tests => 2,
+    cull_delay => .01,
+    handlers => [qw/ TAP /],
+    random => 1,
+    Collector => 'Files',
+    ignore => undef,
+    filetypes => [qw/ Module /],
+    default_asserts => [qw/Core/],
+    default_workflows => [qw/Spec Case Methods/],
+);
+
+Runner->run_tests;
---
+++ t/NamedVar.pm
@@ -0,0 +1,60 @@
+package TEST::Block::NamedVar;
+use strict;
+use warnings;
+use Fennec;
+
+BEGIN {
+    use_ok( 'Block::NamedVar' );
+}
+use Block::NamedVar;
+
+tests 'ngrep' => sub {
+    my @list = ngrep my $x { $x =~ m/^[a-zA-Z]$/ } qw/ a 1 b 2 c 3/;
+    is_deeply(
+        \@list,
+        [qw/a b c/],
+        "filtered as expected"
+    );
+};
+
+tests 'nmap' => sub {
+    my @list = nmap my $x { "updated_$x" } qw/ a b c /;
+    is_deeply(
+        \@list,
+        [qw/updated_a updated_b updated_c/],
+        "mapped as expected"
+    );
+};
+
+tests 'edge' => sub {
+    my @list = ngrep
+        my
+            $x
+                {
+                    $x =~ m/^[a-zA-Z]$/
+                }
+                    qw/ a 1 b 2 c 3/;
+    is_deeply(
+        \@list,
+        [qw/a b c/],
+        "filtered as expected staircased"
+    );
+};
+
+tests 'count and vartypes' => sub {
+    my $count = ngrep my $x { $x =~ m/^[a-zA-Z]$/ } qw/ a 1 b 2 c 3/;
+    is( $count, 3, "counts properly" );
+
+    my $y;
+    $count = ngrep $y { $y =~ m/^[a-zA-Z]$/ } qw/ a 1 b 2 c 3 222/;
+    is( $count, 3, "closure block var" );
+    is( $y, '222', "Used outer scope var" );
+
+    $count = ngrep our $v { $v =~ m/^[a-zA-Z]$/ } qw/ a 1 b 2 c 3/;
+    is( $count, 3, "package block var" );
+
+    $count = ngrep thing { $thing =~ m/^[a-zA-Z]$/ } qw/ a 1 b 2 c 3/;
+    is( $count, 3, "shorthand new variable" );
+};
+
+1;
